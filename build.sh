e7db3fc9b8bfb56c200ab5d810c8a5379f37073c
Atnanasi <ys2000pro@gmail.com>
2017-08-08 10:47:35 +0900

    Don't use mongodb,redis in Misskey-Docker

--- build.sh
+++ build.sh
@@ -10,26 +10,31 @@ mkdir /file
 apk update
 echo "http://dl-cdn.alpinelinux.org/alpine/edge/main" >> /etc/apk/repositories
 echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories
-apk add --no-cache mongodb --allow-untrusted
-apk add --no-cache redis graphicsmagick openssl nodejs python alpine-sdk
+apk add --no-cache graphicsmagick openssl nodejs python alpine-sdk
 npm install -g forever node-gyp
 
-# Build Misskey-Web
+# Get Misskey
 git clone git://github.com/misskey-delta/Misskey-Web.git
-cd Misskey-Web
+git clone git://github.com/misskey-delta/Misskey-API.git
+git clone git://github.com/misskey-delta/Misskey-File.git
+
+# Set config file for build Misskey-Web
+mkdir ~/.misskey
+cp ~/Misskey-Web/.ci/web.json ~/.misskey/web.json
+
+# Build Misskey-Web
+cd ~/Misskey-Web
 npm run deploy
-cd ..
 
 # Build Misskey-API
-git clone git://github.com/misskey-delta/Misskey-API.git
-cd Misskey-API
+cd ~/Misskey-API
 npm install
 npm run build
-cd ..
 
 # Build Misskey-File
-git clone git://github.com/misskey-delta/Misskey-File.git
-cd Misskey-File
+cd ~/Misskey-File
 npm install
 npm run build
-cd ..
+
+# Clean up
+rm -rf ~/.misskey
